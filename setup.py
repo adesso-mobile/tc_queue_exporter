e50143e206d59f0b9c13bdd9c6cfc2c608a067bc
Dennis Dudek <dudek@adesso-mobile.de>
2022-02-21 16:29:35 +0100

    allow bearer token

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tc_queue_exporter",  # Replace with your own username
-    version="0.1.0.dev3",
+    version="0.1.1",
     author="adesso mobile solutions GmbH",
     author_email="it-operations@adesso-mobile.de",
     description="Export teamcity queue data",
